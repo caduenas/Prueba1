b9ceb3c388ba970bd6987ce8bbd15ec099aadf83
caduenas <duenascalos9@gmail.com>
2020-10-09 13:32:24 -0500

    Escritura de datos

--- Estudiante.py
+++ Estudiante.py
@@ -4,7 +4,10 @@ class Estudiante():
         self.__Codigo=codigo
     def getName(self):
         return self.__Nombre
-
+    def setName(self,newName):
+        self.__Nombre = newName
+        return self.__Nombre
 
 Carlos=Estudiante(str(input("Escriba el nombre del estudiante")),str(input("Escriba el codigo del estudiante")))
 print(Carlos.getName())
+print(Carlos.setName(input("Nuevo nombre")))
