1b0740b9264a332f05e0633f5117e025e4561f33
caduenas <duenascalos9@gmail.com>
2020-10-09 12:14:00 -0500

    Cambio de enfoque

--- Archivo.py
+++ Archivo.py
@@ -1,2 +1,5 @@
-print("Hola es una prueba")
-print("Segundo titulo")
+def suma(numero1,numero2):
+    return numero1+numero2
+a = int(input("Escriba el nuemro 1°"))
+b = int(input("Escriba el nuemro 2°"))
+print(suma(a,b))
